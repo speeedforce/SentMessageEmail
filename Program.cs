13ab0f4d344c106d2f956d7a086e3160d99def9e
Stas Zinoviev <zstanislav98@gmail.com>
2017-12-23 19:14:41 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace SentMessageEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // отправитель - устанавливаем адрес и отображаемое в письме имя
+            MailAddress from = new MailAddress("forworkjavar@gmail.com", "System");
+            // кому отправляем
+            MailAddress to = new MailAddress("footballluckySend@gmail.com");
+            // создаем объект сообщения
+            MailMessage m = new MailMessage(from, to);
+            // тема письма
+            m.Subject = "Тест";
+            // текст письма
+            m.Body = "<h2> Уважаемый " + "Стас" + "!</h2>";
+
+            // письмо представляет код html
+            m.IsBodyHtml = true;
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            // логин и пароль
+            smtp.Credentials = new NetworkCredential("forworkjavar@gmail.com", "PPPI2806s");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+        }
+
+
+        void GoogleMethod()
+        {
+
+        }
+    }
+}
